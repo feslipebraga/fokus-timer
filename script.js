5441db60765a2e9f7a70aa4392aaa02cdea25527
feslipebraga <feslipebraga@gmail.com>
2026-03-17 10:50:14 -0300

    feat: add initial timer display and enhance button functionality

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
-const html = document.querySelector('html');
-const focoBtn = document.querySelector('.app__card-button--foco');
-const curtoBtn = document.querySelector('.app__card-button--curto');
-const longoBtn = document.querySelector('.app__card-button--longo');
+const htmlElement = document.querySelector('html');
+const focusButton = document.querySelector('.app__card-button--foco');
+const shortBreakButton = document.querySelector('.app__card-button--curto');
+const longBreakButton = document.querySelector('.app__card-button--longo');
+const playPauseButton = document.querySelector('.app__card-primary-button');
+const timerDisplay = document.querySelector('#timer');
+const focusDuration = 1500;
+const shortBreakDuration = 300;
+const longBreakDuration = 900;
+const mainImage = document.querySelector('.app__image');
+const mainTitle = document.querySelector('.app__title');
 
+focusButton.addEventListener('click', () => {
+    htmlElement.setAttribute('data-contexto', 'foco');
+    // timerDisplay;
+    mainImage.src = '/imagens/foco.png';
+    mainTitle.innerHTML = 'Otimize sua produtividade,<br><strong class="app__title-strong">mergulhe no que importa.</strong>';
+    focusButton.classList.add('active');
+    shortBreakButton.classList.remove('active');
+    longBreakButton.classList.remove('active');
 
-
-focoBtn.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco');
 })
 
-curtoBtn.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto');
+shortBreakButton.addEventListener('click', () => {
+    htmlElement.setAttribute('data-contexto', 'descanso-curto');
+    // timerDisplay;
+    mainImage.src = '/imagens/descanso-curto.png';
+    mainTitle.innerHTML = 'Que tal dar uma respirada?<br><strong class="app__title-strong">Faça uma pausa curta!</strong>';
+    shortBreakButton.classList.add('active');
+    focusButton.classList.remove('active');
+    longBreakButton.classList.remove('active');
 })
 
-longoBtn.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo');
+longBreakButton.addEventListener('click', () => {
+    htmlElement.setAttribute('data-contexto', 'descanso-longo');
+    // timerDisplay;
+    mainImage.src = '/imagens/descanso-longo.png';
+    mainTitle.innerHTML = 'Hora de voltar à superfície.<br><strong class="app__title-strong">Faça uma pausa longa!</strong>';
+    longBreakButton.classList.add('active');
+    focusButton.classList.remove('active');
+    shortBreakButton.classList.remove('active');
+})
+
+playPauseButton.addEventListener('click', () => {
+    alert('Função de iniciar/pausar o timerDisplay ainda não implementada!');
 })
