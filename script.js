a0d185240ab7c592121e2b84f01d326ec6f973c2
feslipebraga <feslipebraga@gmail.com>
2026-03-18 13:31:24 -0300

    refactor: streamline context handling in toggleContext function

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const htmlElement = document.querySelector('html');
 const focusButton = document.querySelector('.app__card-button--foco');
 const shortBreakButton = document.querySelector('.app__card-button--curto');
 const longBreakButton = document.querySelector('.app__card-button--longo');
-const playPauseButton = document.querySelector('.app__card-primary-button');
+
+const playPauseButton = document.querySelector('#start-pause');
 const timerDisplay = document.querySelector('#timer');
+
 const focusDuration = 1500;
 const shortBreakDuration = 300;
 const longBreakDuration = 900;
+
 const mainImage = document.querySelector('.app__image');
 const mainTitle = document.querySelector('.app__title');
 const buttons = document.querySelectorAll('.app__card-button');
@@ -28,26 +31,20 @@ const resetActiveButtons = () => {
 }
 
 const toggleContext = (context) => {
+    htmlElement.setAttribute('data-contexto', context);
+    mainImage.src = `/imagens/${context}.png`;
+    resetActiveButtons();
     switch (context) {
         case 'foco':
-            htmlElement.setAttribute('data-contexto', context);
-            mainImage.src = `/imagens/${context}.png`;
             mainTitle.innerHTML = 'Otimize sua produtividade,<br><strong class="app__title-strong">mergulhe no que importa.</strong>';
-            resetActiveButtons();
             focusButton.classList.add('active');
             break;
         case 'descanso-curto':
-            htmlElement.setAttribute('data-contexto', context);
-            mainImage.src = `/imagens/${context}.png`;
             mainTitle.innerHTML = 'Que tal dar uma respirada?<br><strong class="app__title-strong">Faça uma pausa curta!</strong>';
-            resetActiveButtons();
             shortBreakButton.classList.add('active');
             break;
         case 'descanso-longo':
-            htmlElement.setAttribute('data-contexto', context);
-            mainImage.src = `/imagens/${context}.png`;
             mainTitle.innerHTML = 'Hora de voltar à superfície.<br><strong class="app__title-strong">Faça uma pausa longa!</strong>';
-            resetActiveButtons();
             longBreakButton.classList.add('active')
             break;
     }
@@ -66,5 +63,6 @@ longBreakButton.addEventListener('click', () => {
 });
 
 playPauseButton.addEventListener('click', () => {
-    alert('Função de iniciar/pausar o timerDisplay ainda não implementada!');
-})
+    // a implementar
+});
+
